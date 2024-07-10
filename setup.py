29fc22386ae95bdc30d483498c710a02bd7eb982
Sudip <sudipbhattarai100@gmail.com>
2024-07-10 11:03:28 +0545

    Add publish command to gov-cli

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gov-cli",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=[
         # List your dependencies here, e.g.:
